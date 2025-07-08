a5e8c3336c46ff483fa83df8f9163d2b3dfe46d0
Kimple Ke <jackwarpten@hotmail.com>
2025-07-08 22:31:09 +1000

    add a solution for disappearing pairs question

---
+++ CodingChallengeSolutions/DisappearingPairs/DisappearingPairs.py
@@ -0,0 +1,12 @@
+# author: Kimble Ke
+# date: 2025.7.6
+ 
+# O(N) Time, O(N) Space solution
+def solution(S):
+  stack = []
+  for ch in S:
+    if stack and stack[-1] == ch:
+      stack.pop()  # Remove the pair
+    else:
+      stack.append(ch)  # Add current character
+  return ''.join(stack)
