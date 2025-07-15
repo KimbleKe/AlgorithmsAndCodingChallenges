0de309acfe5fc183be345ef110f6fe06a21d0408
Kimple Ke <jackwarpten@hotmail.com>
2025-07-15 13:37:31 +1000

    add a comment

--- CodingChallengeSolutions/MissingInteger/MissingInteger.py
+++ CodingChallengeSolutions/MissingInteger/MissingInteger.py
@@ -1,10 +1,10 @@
 # author: Kimble Ke
 # date: 2025.7.15
 
-# Time Complexity: O(N)
+# Time Complexity: O(N). Worst case: N+1 is missing
 def missing_integer(A):
-  seen = set(A)
-  for i in range(1, len(A)+2):  # Worst case: N+1 is missing
+  seen = set(A)    # store all positive numbers in a set for O(1) lookup
+  for i in range(1, len(A)+2): 
     if i not in seen:
       return i
     
--- CodingChallengeSolutions/MissingInteger/MissingInteger_debug.py
+++ CodingChallengeSolutions/MissingInteger/MissingInteger_debug.py
@@ -1,9 +1,9 @@
 # author: Kimble Ke
 # date: 2025.7.15
 
-# Time Complexity: O(N), Worst case: N+1 is missing
+# Time Complexity: O(N). Worst case: N+1 is missing
 def missing_integer(A):
-  seen = set(A)
+  seen = set(A) # store all positive numbers in a set for O(1) lookup
   for i in range(1, len(A)+2): 
     if i not in seen:
       return i
