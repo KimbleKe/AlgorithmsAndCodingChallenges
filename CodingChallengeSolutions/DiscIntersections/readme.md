e484d0f1d84eda6eab28f88567f0817b8119b3b9
Kimple Ke <jackwarpten@hotmail.com>
2025-07-20 22:32:11 +1000

    solved DiscIntersections

---
+++ CodingChallengeSolutions/DiscIntersections/readme.md
@@ -0,0 +1,17 @@
+Problem: DiscIntersections
+Task Description:
+Given an array A of N integers where each element A[i] represents the radius of a disc centered at (i, 0) on a 2D plane, compute the number of intersecting discs. If the number exceeds 10,000,000, return -1.
+
+Examples:
+A = [1, 5, 2, 1, 4, 0] → Returns 11
+
+A = [1, 1, 1] → Returns 3
+
+Constraints:
+N is an integer within [0..100,000]
+
+Each element in A is within [0..2,147,483,647]
+
+Time: O(N log N)
+
+Space: O(N)
---
+++ CodingChallengeSolutions/DiscIntersections/solution.py
@@ -0,0 +1,31 @@
+# author: Kimble Ke
+# date: 2025.7.20
+ 
+# O(NlogN) Time, O(N) Space
+def solution1(A):
+  n = len(A)
+  start_points = []
+  end_points = []
+  
+  for i in range(n):
+    start_points.append(i - A[i])
+    end_points.append(i + A[i])
+  
+  start_points.sort()
+  end_points.sort()
+  
+  intersections = 0
+  active_discs = 0
+  end_ptr = 0
+  
+  for start_ptr in range(n):
+    while end_ptr < n and end_points[end_ptr] < start_points[start_ptr]:
+      active_discs -= 1
+      end_ptr += 1
+    intersections += active_discs
+    active_discs += 1
+    
+    if intersections > 10_000_000:
+      return -1
+  
+  return intersections
