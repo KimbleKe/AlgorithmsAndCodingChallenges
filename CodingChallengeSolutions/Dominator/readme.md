e2e0245f178500cc604b97bac29a5a1f5fe897ec
Kimple Ke <jackwarpten@hotmail.com>
2025-07-21 16:52:19 +1000

    solved Dominator

---
+++ CodingChallengeSolutions/Dominator/readme.md
@@ -0,0 +1,17 @@
+Problem: Dominator
+Task Description:
+Given an array A of N integers, find any index of the dominator (a value that occurs in more than half of the elements). Return -1 if no dominator exists.
+
+Examples:
+A = [3, 4, 3, 2, 3, -1, 3, 3] → Returns 0, 2, 4, 6, or 7 (value 3 appears 5/8 times).
+
+A = [1, 2, 3] → Returns -1 (no dominator).
+
+Constraints:
+N is within [0..100,000].
+
+Each element in A is within [−2,147,483,648..2,147,483,647].
+
+Time: O(N)
+
+Space: O(1)
---
+++ CodingChallengeSolutions/Dominator/solution.py
@@ -0,0 +1,36 @@
+# author: Kimble Ke
+# date: 2025.7.21
+ 
+# O(N) Time, O(1) Space
+def solution1(A):
+	candidate = None
+	count = 0
+	# find dominating number 
+	for num in A:
+		if count == 0:
+			candidate = num
+			count = 1
+		else:
+			count += 1 if num == candidate else -1
+	# verify dominator and return dominator index
+	if A.count(candidate) > len(A) // 2:
+		return A.index(candidate)
+	return -1
+
+# O(N) Time, O(N) Space
+def solution2(A):
+	if len(A) == 0:
+		return -1
+	if len(A) == 1:
+		return 0
+	B = A.copy()
+	B.sort()
+	c = 1
+	for i in range(1,len(B)):
+		if B[i] != B[i-1]:
+			c = 1
+		else:
+			c += 1
+		if c > int(len(B)/2):
+			return A.index(B[i])
+	return -1
