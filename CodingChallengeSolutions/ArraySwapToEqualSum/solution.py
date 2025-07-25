b467f51600f6d7bbb5033d75daa5f7fe7d077e55
Kimple Ke <jackwarpten@hotmail.com>
2025-07-25 18:25:33 +1000

    update solution

--- CodingChallengeSolutions/ArraySwapToEqualSum/solution.py
+++ CodingChallengeSolutions/ArraySwapToEqualSum/solution.py
@@ -8,13 +8,12 @@ def solution1(A, B):
 	sumB = sum(B)
 	delta = sumB - sumA
 
-	# If the difference is odd, it's impossible to balance with integer swaps
+	# If the difference is odd, the odd number cannot be divided to shared with both arrays, it's impossible to balance with integer swaps
 	if delta % 2 != 0:
 		return False
 
 	target = delta // 2
 	setB = set(B)
-
 	for a in A:
 		if a + target in setB:
 			return True
