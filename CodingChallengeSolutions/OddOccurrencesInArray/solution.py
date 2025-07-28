ccaa0ca091d86d38c2fb421d343fb9ba1a16499d
Kimple Ke <jackwarpten@hotmail.com>
2025-07-28 15:09:37 +1000

    update solution

--- CodingChallengeSolutions/OddOccurrencesInArray/solution.py
+++ CodingChallengeSolutions/OddOccurrencesInArray/solution.py
@@ -1,10 +1,16 @@
 # author: Kimble Ke
 # date: 2025.7.15
  
-# Time Complexity O(N)
+# Time Complexity: O(N)
+# Space Complexity: O(1)
 def solution(A):
+  """
+  Property of XOR:
+    a ^ a = 0 (Any number XOR itself cancels out)
+    a ^ 0 = a (XOR with 0 returns the original number)
+    XOR is commutative and associative (a ^ b ^ a = (a ^ a) ^ b = 0 ^ b = b)
+  """
   result = 0
   for num in A:
-    result ^= num  # XOR all elements
+    result ^= num  # bitwise XOR operation between two integers across all elements
   return result
-
