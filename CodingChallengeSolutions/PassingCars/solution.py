cee1a8f294ceeedb9fb4129fc25d9aeff26d1658
Kimple Ke <jackwarpten@hotmail.com>
2025-07-29 13:52:20 +1000

    add alternative solution to a problem

--- CodingChallengeSolutions/PassingCars/solution.py
+++ CodingChallengeSolutions/PassingCars/solution.py
@@ -15,4 +15,22 @@ def solution(A):
         return -1
   return passing_pairs
 
+# O(N) Time, O(N) Space
+def solution1(A):
+  n = len(A)
+  prefix = [0]*(n+1)
+  count = 0
+
+  # compute prefix sums (count of west passing cars)
+  for i in range(1, n+1):
+    prefix[i] = prefix[i-1] + A[i-1]
+
+  # compute passing pairs, for each east passing car, add the number of west passing cars after it
+  for i in range(n):
+    if A[i] == 0: # east passing car
+      count += prefix[-1] - prefix[i+1]
+      if count > 1_000_000_000:
+        return -1
+      
+  return count
 
