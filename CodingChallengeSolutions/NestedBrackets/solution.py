cfa36412dcc69db50e88b2cce2d56fdd1e05f41d
Kimple Ke <jackwarpten@hotmail.com>
2025-08-03 14:47:41 +1000

    update solution

--- CodingChallengeSolutions/NestedBrackets/solution.py
+++ CodingChallengeSolutions/NestedBrackets/solution.py
@@ -1,18 +1,19 @@
 # author: Kimble Ke
 # date: 2025.7.21
  
-# O(N) Time, O(N) Space
+# Time complexity - O(N)
+# Space complexity - O(N) 
 def solution(S):
   stack = []
-  bracket_pairs = {')': '(', '}': '{', ']': '['}
+  bracket_pairs = {'(': ')', '{': '}', '[': ']'}
   
   for char in S:
-    if char in bracket_pairs.values():  # Opening bracket
+    if char in bracket_pairs.keys():  # opening bracket
       stack.append(char)
-    elif char in bracket_pairs.keys():  # Closing bracket
-      if not stack or stack.pop() != bracket_pairs[char]:
+    elif char in bracket_pairs.values():  # closing bracket
+      if not stack or bracket_pairs[stack.pop()] != char:
         return 0
-    else:  # Invalid character (optional, if input is guaranteed clean)
+    else:  # invalid character (optional, if input is guaranteed clean)
       return 0
   
   return 1 if not stack else 0
