2aac38e90f462400f2e369d080fe1da4b908ea6f
Kimple Ke <jackwarpten@hotmail.com>
2025-08-15 14:37:12 +1000

    add problem

---
+++ CodingChallengeSolutions/RoadRollerDrivesPatchPotholes/readme.md
@@ -0,0 +1,16 @@
+Problem:
+
+Road roller drives required to patch potholes
+
+e.g. 
+X=[2,4,2,6,7,1], Y=[0,5,3,2,1,5], W=2, Ans=3
+X=[4,8,2,2,1,4], Y=[1,2,3,1,2,3], W=3, Ans=2
+X=[0,3,6,5], Y=[0,3,2,4], W=1, Ans=3
+
+
+<br><br><br>
+
+> **Difficulty level**
+> medium
+
+
---
+++ CodingChallengeSolutions/RoadRollerDrivesPatchPotholes/solution_debug.py
@@ -0,0 +1,22 @@
+from solution import solution
+
+# print input and output
+if __name__ == "__main__":
+  import sys
+
+  if len(sys.argv) != 2:
+    sys.exit(1)
+
+  input_file = sys.argv[1]
+  with open(input_file, 'r') as f:
+    line = f.read().strip()
+    A,B,W = eval(line)
+
+  print("######## input ########")
+  print("input=" + str(A) + ", " + str(B) + ", W=" + str(W))
+
+  result = solution(A,B,W)
+
+  print("######## result ########")
+  print("result is " + str(result))
+
